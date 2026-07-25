0c1103688254f30bbe35b0b6011ba7a4a3dbf0fe
Chanthini Mai <chanthinivenkatesh20@gmail.com>
2026-07-25 22:42:40 +0530

    original

--- server/server.js
+++ server/server.js
@@ -62,3 +62,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`));
+
+
